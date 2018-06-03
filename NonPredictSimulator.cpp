e2f403a5d01db8870a1cd275321b1db1c8899cf7
seahime <haohan1314@gmail.com>
2018-06-03 16:43:51 +0800

    init NonPredictSimulator

---
+++ NonPredictSimulator.cpp
@@ -0,0 +1,1 @@
+#include "NonPredictSimulator.h"
---
+++ NonPredictSimulator.h
@@ -0,0 +1,15 @@
+/**
+ * LRUCache without prediction algorithm
+ * */
+
+#ifndef MINER_NONPREDICTSIMULATOR_H
+#define MINER_NONPREDICTSIMULATOR_H
+
+#include "Simulator.h"
+
+class NonPredictSimulator : public Simulator {
+
+};
+
+
+#endif //MINER_NONPREDICTSIMULATOR_H
